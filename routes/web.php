f8ddf2e418b1e13a17e3fc2f0ebee156b4f2056c
Dev-Sooraj-Git <mackarm731@gmail.com>
2026-02-22 01:13:11 +0530

    added usefull comment for root dir

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BooksController;
 
-Route::redirect('/', '/authors');
+Route::redirect('/', '/authors'); // onload of root of site, redirect to authors
 Route::resource('authors', AuthorController::class); // routes for authors CRUD
 Route::resource('books', BooksController::class); //routes for books CRUD
 
